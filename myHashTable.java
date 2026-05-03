49f97d2dc553d539d309b1bd7ad8e7a374d4f86c
Aruzhan2208 <aruzanasylkanova@gmail.com>
2026-05-03 18:20:30 +0500

    Implement MyHashTable class with basic operations

---
+++ myHashTable.java
@@ -0,0 +1,102 @@
+public class MyHashTable<K, V> {
+    private class HashNode<K, V> {
+        K key;
+        V value;
+        HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private HashNode<K, V>[] chainArray;
+    private int M = 11; 
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public MyHashTable() {
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public MyHashTable(int M) {
+        this.M = M;
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+
+    private int hash(K key) {
+        // Защита от отрицательных значений
+        return Math.abs(key.hashCode() % M);
+    }
+
+    public void put(K key, V value) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+        while (head != null) {
+            if (head.key.equals(key)) {
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        size++;
+        head = chainArray[index];
+        HashNode<K, V> newNode = new HashNode<>(key, value);
+        newNode.next = head;
+        chainArray[index] = newNode;
+    }
+
+    public V get(K key) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = hash(key);
+        HashNode<K, V> head = chainArray[index];
+        HashNode<K, V> prev = null;
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                break;
+            }
+            prev = head;
+            head = head.next;
+        }
+
+        if (head == null) return null; 
+
+        size--;
+        if (prev != null) {
+            prev.next = head.next;
+        } else {
+            chainArray[index] = head.next;
+        }
+
+        return head.value;
+    }
+    public int[] getBucketSizes() {
+        int[] sizes = new int[M];
+        for (int i = 0; i < M; i++) {
+            int count = 0;
+            HashNode<K, V> head = chainArray[i];
+            while (head != null) {
+                count++;
+                head = head.next;
+            }
+            sizes[i] = count;
+        }
+        return sizes;
+    }
+}
